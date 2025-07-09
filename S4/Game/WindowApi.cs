465b27e8f43af1a5ed717c8560ac8da4735f51a5
WizzardMaker <wizzardmaker@gmail.com>
2025-07-09 17:21:11 +0200

    added window api

---
+++ S4/Game/WindowApi.cs
@@ -0,0 +1,43 @@
+﻿using AutomaticInterface;
+
+using Forge.Native;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forge.S4.Game {
+
+    /// <summary>
+    /// An interface for accessing window-related functionality.
+    /// 
+    /// Fetching the screen size and listening for changes in the screen size.
+    /// </summary>
+    public partial interface IWindowApi { }
+
+    [GenerateAutomaticInterface]
+    internal sealed class WindowApi : IWindowApi {
+        public WindowApi() {
+            User32.AddWndProc(EventHandler);
+        }
+
+        private bool EventHandler(WndProcMsg msg, UIntPtr wParam, UIntPtr lParam) {
+            if (msg == WndProcMsg.WM_SIZE) {
+                ScreenSizeChanged?.Invoke(GetScreenSize());
+            }
+
+            return false;
+        }
+
+        public Vector2 GetScreenSize() {
+            User32.GetClientRect(GameValues.Hwnd, out User32.Rect rect);
+
+            return new Vector2(rect.Z - rect.X, rect.W - rect.Y);
+        }
+
+        public event Action<Vector2> ScreenSizeChanged;
+    }
+}
